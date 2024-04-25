00592ad87158e99a02789ccf40b3ca331229d7c9
Brandon <brandonslee3@gmail.com>
2024-04-25 13:34:42 -0400

    complete lab

--- lib/song.py
+++ lib/song.py
@@ -1,2 +1,27 @@
 class Song:
-    pass
+
+    count = 0
+    genres = []
+    artists = []
+    artist_count = {}
+    genre_count = {}
+    
+    def __init__(self, name:str, artist:str, genre:str) -> None:
+        self.name = name
+        self.artist = artist
+        self.genre = genre
+        Song.count += 1
+        Song.artists.append(self.artist)
+        Song.genres.append(self.genre)
+
+        if Song.artist_count.get(f"{self.artist}") == None:
+            Song.artist_count[f"{self.artist}"] = 1
+        elif Song.artist_count.get(f"{self.artist}") != None:
+            Song.artist_count[f"{self.artist}"] += 1
+
+        if Song.genre_count.get(f"{self.genre}") == None:
+            Song.genre_count[f"{self.genre}"] = 1
+        elif Song.genre_count.get(f"{self.genre}") != None:
+            Song.genre_count[f"{self.genre}"] += 1
+
+    
